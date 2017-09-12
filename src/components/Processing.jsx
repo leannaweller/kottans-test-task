74567d63c43cbd9853e76aeefa13e87f494ad629
leannaweller <liliya.lyubarskaya@gmail.com>
2017-09-12 17:13:34 +0300

    updated styles

--- src/components/Processing.jsx
+++ src/components/Processing.jsx
@@ -10,7 +10,8 @@ class Processing extends Component {
   constructor(props){
     super(props);
     this.state = {
-      open: false
+      open: false,
+      showForm : false
     }
   }
   handleApply = (e) => {
@@ -30,15 +31,25 @@ class Processing extends Component {
     route(`/${encodeURI(profile)}`);
     window.location.reload();
   }
-  render({data},{open}){
+  render({data},{open,showForm}){
     return(
       <div className="processing">
-        <form >
-          <Filter langs={utils.getLangs(data)}/>
-          <Sorting/>
-          <button className="btn-submit" type="submit" onClick={this.handleApply}>Apply</button>
-          <button className="btn-submit" type="submit" onClick={this.hadleClear}>Clear</button>
-        </form>
+        {
+          showForm &&
+          <form >
+            <h4>Filters:</h4>
+            <Filter langs={utils.getLangs(data)}/>
+            <h4>Sorting:</h4>
+            <Sorting/>
+            <button className="btn-submit" type="submit" onClick={this.handleApply}>Apply</button>
+            <button className="btn-submit" type="submit" onClick={this.hadleClear}>Clear</button>
+          </form>
+
+        }
+          <button className="btn-submit" onClick={()=>{this.setState({showForm:!showForm})}}>
+            {showForm ? 'Hide filter setting' : 'Open filter settings'}
+          </button>
+
         <Modal open={open} onClose={()=>{this.setState({open:false})}}>
           <h1>Oops..Error...</h1>
         </Modal>
